39bbb2258a6ab4ae6a0af40ec8278dea65a7ad05
Josh O'Connell <j.o.connell95@gmail.com>
2017-10-25 14:49:05 +0100

    lab 2 complete

---
+++ lab1/lab1/ConsoleApplication1/Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Net.h"
+#include <string>
+#include "Player.h"
+int main()
+{
+	//Net net = Net();
+	//net.initialise();
+	std::string x;
+	std::cin >> x;
+
+	if (x == "a")
+	{
+
+	}
+	if (x == "b")
+	{
+
+	}
+	system("PAUSE");
+	return 0;
+
+}
